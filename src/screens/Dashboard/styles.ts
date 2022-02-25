e353da99889a41d3f37880ac65a669fec35e6349
Talisson Oliveira <talissonlux@outlook.com>
2022-02-25 15:01:02 -0300

    feat: create global styles and theme

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,10 +1,13 @@
 import styled from 'styled-components/native'
 
 export const Container = styled.View`
+  background-color: ${({ theme }) => theme.colors.primary};
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: red;
 `
 
-export const Title = styled.Text``
+export const Title = styled.Text`
+  font-size: 24px;
+  color: ${({ theme }) => theme.colors.title};
+`
