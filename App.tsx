3ca42558e9e4eb704b35cdbf35dab4836d6abe83
Talisson Oliveira <talissonlux@outlook.com>
2022-02-28 18:42:13 -0300

    feat: creates the transaction record screen header

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import theme from './src/global/styles/theme'
 import AppLoading from 'expo-app-loading'
 import { ThemeProvider } from 'styled-components'
 import { Dashboard } from './src/screens/Dashboard'
+import { Register } from './src/screens/Register'
 import {
   useFonts,
   Poppins_400Regular,
@@ -24,7 +25,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <Register />
     </ThemeProvider>
   )
 }
---
+++ src/screens/Register/styles.ts
@@ -0,0 +1,22 @@
+import styled, { ThemeConsumer } from 'styled-components/native'
+import { RFValue } from 'react-native-responsive-fontsize'
+
+export const Container = styled.View`
+  
+`
+
+export const Header = styled.View`
+  background-color: ${({ theme }) => theme.colors.primary};
+  width: 100%;
+  height: ${RFValue(113)}px;
+
+  align-items: center;
+  justify-content: flex-end;
+  padding-bottom: 19px;
+`
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(18)}px;
+`
