2041ffeda6a362a88eb74c87ed046a9e938b68ae
Talisson Oliveira <talissonlux@outlook.com>
2022-03-01 20:03:12 -0300

    feat: create form validation with yup

--- src/components/Forms/InputForm/index.tsx
+++ src/components/Forms/InputForm/index.tsx
@@ -2,16 +2,18 @@ import React from 'react'
 import { TextInputProps } from 'react-native'
 import { Control, Controller } from 'react-hook-form'
 import { Input } from '../Input'
-import { Container } from './styles'
+import { Container, Error } from './styles'
 
 interface InputFormProps extends TextInputProps {
   control: Control
   name: string
+  error: string
 }
 
 export function InputForm({
   control,
   name,
+  error,
   ...rest
 }: InputFormProps) {
   return (
@@ -27,6 +29,8 @@ export function InputForm({
         )}
         name={name}
       />
+
+      {error && <Error>{error}</Error>}
     </Container>
   )
 }
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
-import { Modal } from 'react-native'
+import { Alert, Modal } from 'react-native'
 import { useForm } from 'react-hook-form'
+
+import * as Yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 
 import { CategorySelectButton } from '../../components/CategorySelectButton'
 import { Button } from '../../components/Forms/Button'
@@ -14,6 +17,14 @@ interface FormData {
   [name: string]: any
 }
 
+const schema = Yup.object().shape({
+  name: Yup.string().required('Insira um nome'),
+  amount: Yup
+    .number()
+    .typeError('Informe um valor válido')
+    .positive('O valor não pode ser negativo')
+})
+
 export function Register() {
   const [transactionType, setTransactionType] = useState('')
   const [isCategoryModalOpen, setIsCategoryModalOpen] = useState(false)
@@ -24,8 +35,11 @@ export function Register() {
 
   const {
     control, // registrar os inputs do formulário
-    handleSubmit // pegar todos os valores dos inputs do formulário e enviar
-  } = useForm()
+    handleSubmit, // pegar todos os valores dos inputs do formulário e enviar
+    formState: { errors } // capturar os erros do formulário
+  } = useForm({
+    resolver: yupResolver(schema)
+  })
 
   function handleTransactionsTypesSelect(type: 'income' | 'outcome') {
     setTransactionType(type)
@@ -40,6 +54,14 @@ export function Register() {
   }
 
   function handleRegister(form: FormData) {
+    if (!transactionType) {
+      return Alert.alert('Selecione o tipo da transação')
+    }
+
+    if (category.key === 'category') {
+      return Alert.alert('Selecione a categoria da transação')
+    }
+
     const data = {
       name: form.name,
       amount: form.amount,
@@ -61,11 +83,16 @@ export function Register() {
             control={control}
             name="name"
             placeholder="Nome"
+            autoCapitalize="sentences"
+            autoCorrect={false}
+            error={errors.name && errors.name.message}
           />
           <InputForm
             control={control}
             name="amount"
             placeholder="Preço"
+            keyboardType="numeric"
+            error={errors.amount && errors.amount.message}
           />
 
           <TransactionsTypes>
