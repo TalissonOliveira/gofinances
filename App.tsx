c650ba70b3a4e98be38cde6079350076f20309aa
Talisson Oliveira <talissonlux@outlook.com>
2022-03-01 20:27:34 -0300

    feat: add tab navigation from react navigation

--- App.tsx
+++ App.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import theme from './src/global/styles/theme'
 import AppLoading from 'expo-app-loading'
 import { ThemeProvider } from 'styled-components'
-import { Dashboard } from './src/screens/Dashboard'
-import { Register } from './src/screens/Register'
 import {
   useFonts,
   Poppins_400Regular,
   Poppins_500Medium,
   Poppins_700Bold
 } from '@expo-google-fonts/poppins'
+
+import theme from './src/global/styles/theme'
+
+import { NavigationContainer } from '@react-navigation/native'
+import { AppRoutes } from './src/routes/app.routes'
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({
@@ -25,7 +27,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
