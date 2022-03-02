4dbb3423bdaff5a099e248b945e0c1acad749fab
Talisson Oliveira <talissonlux@outlook.com>
2022-03-02 19:53:26 -0300

    feat: show new transaction in listing screen with useFocusEffect

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useFocusEffect } from '@react-navigation/native'
 import { HighlightCard } from '../../components/HighlightCard'
 import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard'
 import {
@@ -60,6 +61,10 @@ export function Dashboard() {
   useEffect(() => {
     loadTransactions()
   }, [])
+
+  useFocusEffect(useCallback(() => {
+    loadTransactions()
+  }, []))
 
   return (
     <Container>
