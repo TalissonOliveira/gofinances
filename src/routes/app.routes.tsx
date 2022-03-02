15a45f72d5407679fcc1d18ba8c81520a073833c
Talisson Oliveira <talissonlux@outlook.com>
2022-03-01 22:13:39 -0300

    feat: style the tab navigation

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { Platform } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import { useTheme } from 'styled-components'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Dashboard } from '../screens/Dashboard'
 import { Register } from '../screens/Register'
@@ -6,21 +9,61 @@ import { Register } from '../screens/Register'
 const { Navigator, Screen } = createBottomTabNavigator()
 
 export function AppRoutes() {
+  const theme = useTheme() // usar o tema do styled-components
+
   return (
-    <Navigator>
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarStyle: {
+          height: 72,
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0
+        }
+      }}
+    >
       <Screen
         name="Listagem"
         component={Dashboard}
+        options={{
+          tabBarIcon: (({ size, color }) =>
+            <Feather
+              name="list"
+              color={color}
+              size={size}
+            />
+          )
+        }}
       />
 
       <Screen
         name="Cadastrar"
         component={Register}
+        options={{
+          tabBarIcon: (({ size, color }) =>
+            <Feather
+              name="dollar-sign"
+              color={color}
+              size={size}
+            />
+          )
+        }}
       />
 
       <Screen
         name="Resumo"
         component={Register}
+        options={{
+          tabBarIcon: (({ size, color }) =>
+            <Feather
+              name="pie-chart"
+              color={color}
+              size={size}
+            />
+          )
+        }}
       />
     </Navigator>
   )
